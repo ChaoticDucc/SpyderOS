52d08d8f5426f0c7e0401f1b9beea02f4b4e4b75
ChaoticDucc <63168091+ChaoticDucc@users.noreply.github.com>
2020-12-18 23:04:56 +0100

    QoL Changes (see description)
    * Added "ex" infront of execute defenitions
    * Removed gethelp
    * Added Rounding

--- SpyderOS.py
+++ SpyderOS.py
@@ -21,8 +21,8 @@ yes=["y", "yes"]
 no=["n", "no"]
 ##data
 items = []
-##execution
-def pow(t):
+##execute
+def expow(t):
     if t == 0:
         print("Starting Up")
         time.sleep(.5)
@@ -55,30 +55,29 @@ def pow(t):
         time.sleep(0.5)
         sys()
         print(s)
-def err(t):
+def exerr(t):
     e="Error "
     c=str(t)
     s=" > "
     if t==0:
-        print(e+c+s+"Unkown")
+        print(e,c,s,"Unkown")
     elif t==1:
-        print(e+c+s+"Invalid Syntax")
+        print(e,c,s,"Invalid Syntax")
     elif t==2:
-        print(e+c+s+"Not Found")
+        print(e,c,s,"Not Found")
     elif t==3:
-        print(e+c+s+"Incorrect Response")
+        print(e,c,s,"Incorrect Response")
     elif t==4:
-        print(e+c+s+"User Input does not exist")
-def help(t):
+        print(e,c,s,"User Input does not exist")
+def exhelp(t):
     if t=="sys":
         print('''
-gethelp: get help on SpyderOS
 about: about SpyderOS
 power: display power options
 num: complete operatons with numbers
 time: complete operations with time
 list: view and edit a list of items''')
-    elif t=="gethelp":
+    elif t=="viewex":
         print('''
 commands: search for commands associated with an app''')
     elif t=="about":
@@ -106,88 +105,45 @@ view: look at the list
 add: add an item to the list
 remove: remove an item from the list''')
     else:
-        err(2)
+        exerr(2)
     print('''
 Works everywhere: 
 help: show list of commands
 quit: quit current app (use in sys to quit os)
-q: alternative to quit''')
-#system
+q: alternative to quit
+
+For more information, visit www.github.com/chaoticducc/spyderos/wiki''')
+#programs
+##core
 def sys():
     n="sys"
     while True:
         x = input(">> ")
-        try:
-            if x == "help":
-                help(n)
-            elif x == "gethelp":
-                gethelp()
-                break
-            elif x == "about":
-                about()
-                break
-            elif x == "num":
-                num()
-                break
-            elif x == "time":
-                times()
-                break
-            elif x == "list":
-                list()
-                break
-            elif x == "power" or x in q:
-                power()
-                break
-            else:
-                err(2)
-        except:
-            err(0)
-def gethelp():
-    n="gethelp"
-    while True:
-        x = input(pn+n+sn)
         if x == "help":
-            help(n)
-        elif x == "commands":
-            print(uq)
-            while True:
-                s = input("Search: ")
-                if s in q:
-                    break
-                else:
-                    try:
-                        help(s)
-                    except:
-                        err(2)
-        elif x == "errcodes":
-            print(uq)
-            while True:
-                s = input("Search: ")
-                if s in q:
-                    break
-                else:
-                    try:
-                        err(s)
-                    except:
-                        err(2)
-        elif x == "variables":
-            print("To quit use the following")
-            print(q)
-            print('''To say "yes" use the following''')
-            print(yes)
-            print('''To say "no" use the following''')
-            print(no)
-        elif x in q:
-            sys()
+            exhelp(n)
+        elif x == "about":
+            about()
+            break
+        elif x == "num":
+            num()
+            break
+        elif x == "time":
+            times()
+            break
+        elif x == "list":
+            list()
+            break
+        elif x == "power" or x in q:
+            power()
             break
         else:
-            err(2)
+            exerr(2)
 def about():
     n="about"
     while True:
         x = input(pn+n+sn)
         if x == "help":
-            help(n)
+            exhelp(n)
         elif x == "version":
             print(version)
         elif x == "credits":
@@ -202,41 +158,38 @@ def about():
             sys()
             break
         else:
-            err(2)
+            exerr(2)
 def power():
     n="power"
     while True:
         x = input(pn+n+sn)
         if x == "help":
-            help(n)
+            exhelp(n)
         elif x == "shutdown":
-            pow(1)
+            expow(1)
             break
         elif x == "restart":
             print("")
-            pow(1)
+            expow(1)
             time.sleep(2)
             print("")
-            pow(0)
+            expow(0)
             break
         elif x == "sleep":
-            pow(2)
+            expow(2)
             break
-#        elif x == "x":
-#            print("done")
-#            break
         elif x in q:
             sys()
             break
         else:
-            err(2)
-#programs
+            exerr(2)
+##tools
 def num():
     n="num"
     while True:
         x = input(pn+n+sn)
         if x == "help":
-            help(n)
+            exhelp(n)
         elif x == "math":
             print('''
 Use + to add
@@ -257,7 +210,7 @@ Use s to reuse previous result''')
                         s = eval(r)
                         print(s)
                     except:
-                        err(1)
+                        exerr(1)
         elif x == "rand": 
             n1 = input("Minimum Number: ")
             r1 = int(n1)
@@ -265,17 +218,24 @@ Use s to reuse previous result''')
             r2 = int(n2)
             r = random.randint(r1,r2)
             print(r)
+        elif x == "round":
+            n1 = input("Number to Round: ")
+            r1 = float(n1)
+            n2 = input("To the nearest _: ")
+            r2 = int(n2)
+            r = round(r1,r2)
+            print(r)
         elif x in q:
             sys()
             break
         else:
-            err(2)
+            exerr(2)
 def times():
     n="time"
     while True:
         x = input(pn+n+sn)
         if x == "help":
-            help(n)
+            exhelp(n)
         elif x == "timer":
             print('''
 Use m for minutes
@@ -309,7 +269,7 @@ Use s for seconds''')
                         min -= 1
                         time.sleep(1)
             else:
-                err(3)
+                exerr(3)
         elif x == "current":
             t = time.localtime()
             cd = time.strftime("%B %d, %Y", t)
@@ -320,21 +280,27 @@ Use s for seconds''')
             sys()
             break
         else:
-            err(2)
+            exerr(2)
 def list():
     n="list"
     while True:
         x = input(pn+n+sn)
         if x == "help":
-            help(n)
+            exhelp(n)
         elif x == "view":
-            print(items)
+            if len(items) == 0:
+                print("List Empty")
+            else:
+                for i in range(0,len(items)):
+                    print(i,items[i])
         elif x == "add":
             print(uq)
             while True:
                 i = input()
                 if i in q:
                     break
+                elif i == int():
+                    items.pop(i)
                 else:
                     items.append(i)
         elif x == "remove":
@@ -346,10 +312,10 @@ def list():
                 elif i in items:
                     items.remove(i)
                 else:
-                    err(4)
+                    exerr(4)
         elif x in q:
             sys()
             break
         else:
-            err(2)
-pow(0)
+            exerr(2)
+expow(0)
